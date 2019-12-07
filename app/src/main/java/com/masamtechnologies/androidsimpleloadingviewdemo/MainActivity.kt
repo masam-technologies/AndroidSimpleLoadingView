1935dfbfb134b32a1539a38bad7b9e7bce4bb61c
Meshack Masam <meshackmasam@gmail.com>
2019-12-07 09:51:33 +0800

    style update

--- app/src/main/java/com/masamtechnologies/androidsimpleloadingviewdemo/MainActivity.kt
+++ app/src/main/java/com/masamtechnologies/androidsimpleloadingviewdemo/MainActivity.kt
@@ -2,11 +2,12 @@ package com.masamtechnologies.androidsimpleloadingviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        simple_loader.start()
     }
 }
